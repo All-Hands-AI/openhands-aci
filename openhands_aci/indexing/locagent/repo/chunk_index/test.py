f76fa79fe6b6fd0933ffc4ccc6985c26fcb32661
czlll <1781750070@qq.com>
2025-05-18 23:49:54 -0700

    update query_file

--- openhands_aci/indexing/locagent/repo/chunk_index/test.py
+++
@@ -1,6 +0,0 @@
-from code_retriever import build_code_retriever_from_repo as build_code_retriever
-retriever = build_code_retriever(
-    '/home/gangda/workspace/czl/projects/LocAgent_anom',
-    persist_path='/home/gangda/workspace/czl/projects/try/index',
-    similarity_top_k=10,
-)
