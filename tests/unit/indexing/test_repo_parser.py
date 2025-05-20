700a8f5addc6f481e33d089ed6e89e8444e4417a
Hoang Tran <descience.thh10@gmail.com>
2025-05-20 07:44:20 +0000

    fix test

--- tests/unit/indexing/test_repo_parser.py
+++
@@ -1,9 +0,0 @@
-from openhands_aci.indexing.locagent.repo.chunk_index.code_retriever import (
-    build_code_retriever_from_repo as build_code_retriever
-)
-
-retriever = build_code_retriever(
-    repo_path='/home/czl/workspace/projects/LocAgent',
-    persist_path='/home/czl/workspace/projects/try/index',
-    similarity_top_k=10,
-)
