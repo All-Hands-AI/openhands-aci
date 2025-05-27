8867f7459dc12c11c09061ff298830a61e087ed0
Hoang Tran <descience.thh10@gmail.com>
2025-05-27 17:07:24 +0000

    assert file content present in md

--- tests/integration/editor/test_view_supported_binary_files.py
+++ tests/integration/editor/test_view_supported_binary_files.py
@@ -14,3 +14,6 @@ def test_view_pdf_file():
     assert isinstance(result, CLIResult)
     assert f"Here's the content of the file {test_file}" in result.output
     assert 'displayed in Markdown format' in result.output
+
+    # Check for specific content present in the PDF
+    assert 'Printer-Friendly Caltrain Schedule' in result.output
